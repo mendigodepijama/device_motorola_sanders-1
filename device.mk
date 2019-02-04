5bbb1eeb07c20945b2bc94233a042bf978865345
Jadilson Guedes <jadilson12@gmail.com>
2019-02-03 23:51:00 -0200

    Revert "sanders: Convert libinit to init scripts"
    This reverts commit 5db228eca77d1e71ee0b4360ca32c7bcc3150114.

--- device.mk
+++ device.mk
@@ -332,8 +332,7 @@ PRODUCT_PACKAGES += \
     init.mmi.rc \
     init.mmi.usb.rc \
     init.qcom.rc \
-    init.qcom.sensors.sh \
-    init.sanders.rc
+    init.qcom.sensors.sh
 
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/rootdir/etc/ueventd.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
