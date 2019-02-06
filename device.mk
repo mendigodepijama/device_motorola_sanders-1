6dbd5cc59d80b8c7236ff80e74de6fec0c76795c
Ashwin R C <ashwin2001achu@gmail.com>
2019-02-06 16:18:18 +0000

    sanders: build android.hidl.manager-V1.0-java
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- device.mk
+++ device.mk
@@ -448,11 +448,13 @@ PRODUCT_COPY_FILES += \
 # TEMP FIX
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+   android.hidl.manager-V1.0-java
 
 # Wi-Fi Display
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
