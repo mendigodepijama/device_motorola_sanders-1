d2427179b37aea47909bdfa726e5e3b08d997500
mendigodepijama <mendigodepijama@gmail.com>
2019-05-14 16:46:18 -0300

    check-features - Remove files if device don't support (dtv, nfc or compass)

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -12,18 +12,28 @@ if [ "$sku" = "XT1806" ]; then
     # XT1806 doesn't have NFC chip
     rm /vendor/etc/permissions/android.hardware.nfc.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
     rm /vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
+else
+    # Only XT1806 variant got a compass
+    rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
 
 if ! [ "$sku" = "XT1802" ]; then
     # Others variants doesn't have DTV support
     rm /vendor/etc/permissions/com.motorola.hardware.dtv.xml
     rm /vendor/etc/permissions/mot_dtv_permissions.xml
+    rm /vendor/bin/hw/motorola.hardware.tv@1.0-service
+    rm /vendor/etc/init/motorola.hardware.tv@1.0-service.rc
     rm /vendor/lib/libdtvtuner.so
+    rm /vendor/lib/hw/motorola.hardware.tv@1.0-impl.so
+    rm /vendor/lib/motorola.hardware.tv@1.0.so
+    rm /vendor/lib/motorola.hardware.tv@1.0_vendor.so
     rm /vendor/lib64/libdtvtuner.so
-    rm /vendor/lib/libdtvhal.so
-    rm /vendor/lib64/libdtvhal.so
+    rm /vendor/lib64/hw/motorola.hardware.tv@1.0-impl.so
+    rm /vendor/lib64/motorola.hardware.tv@1.0.so
+    rm /vendor/lib64/motorola.hardware.tv@1.0_vendor.so
     rm -r /vendor/app/DTVPlayer
     rm -r /vendor/app/DTVService
 fi
