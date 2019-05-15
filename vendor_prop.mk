4ac19539e37537b8382adf0049bdcb906efbd888
mendigodepijama <mendigodepijama@gmail.com>
2019-05-14 23:25:54 -0300

    remove duplicated prop

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -151,8 +151,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.enable.sglscale=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
-    vendor.display.disable_skip_validate=1 \
-    debug.sf.latch_unsignaled=1 \
+    vendor.display.disable_skip_validate=1
 
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
